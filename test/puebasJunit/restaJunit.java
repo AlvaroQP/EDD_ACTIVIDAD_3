12d5a7b0ca233501d10250369e22c8161b54253a
MiguelALozano <miguelartedigital@gmail.com>
2021-02-22 11:25:08 +0100

    Creado classpath y carpeta test

---
+++ test/puebasJunit/restaJunit.java
@@ -0,0 +1,14 @@
+package puebasJunit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class restaJunit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
