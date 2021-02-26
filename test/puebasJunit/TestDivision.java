a6c4b641f5d8a93137a55b445aa51472a3e6000f
JoseASantoss <j.santos.sanchez@gmail.com>
2021-02-26 10:59:23 +0100

    Modifico el testDivision para que quede con el mismo formato y presentación que el testProducto. Dejo declaradas e inicializadas las varibles al principio y las elimino del BeforEach y el AfterEach. También elimino las variables r1 y r2, por redundancia y opero directamente con las variables creadas al inicio. En cada test se creará una variable realNegativo, enteroNegativo y result, según se vayan necesitando.

--- test/puebasJunit/TestDivision.java
+++ test/puebasJunit/TestDivision.java
@@ -54,40 +54,32 @@ public class TestDivision {
 	@Test
 	public void testDivReales() {
 		System.out.println("***** Test metodo divReales *****");
-		double r1 = real1;
-		double r2 = real2;
-		double result = r1/r2;
 		
 		//Se pasan dos valores reales positivos y tiene que devolver el valor de 
 		//la operacion
-		assertEquals(result, Division.divReales(r1,r2));
+		double result = real1/real2;
+		assertEquals(result, Division.divReales(real1,real2));
 		
 		//Se pasa el primer valor como negativo y el segundo como positivo y tiene
 		//que devolver 0
-		r1 = -r1;
-		assertEquals(CERO, Division.divReales(r1, r2));
+		double realNegativo1=-real1;
+		assertEquals(CERO, Division.divReales(realNegativo1, real2));
 		
 		//Se pasa el primer valor como positivo y el segundo como negativo y tiene que
 		//devolver 0
-		r1 = real1;
-		r2 = -r2;
-		assertEquals(CERO, Division.divReales(r1,r2));
+		double realNegativo2=-real2;
+		assertEquals(CERO, Division.divReales(real1,realNegativo2));
 		
 		//Se pasan los dos valores negativos y tiene que devolver 0
-		r1 = -r1;
-		assertEquals(CERO, Division.divReales(r1, r2));
+		assertEquals(CERO, Division.divReales(realNegativo1, realNegativo2));
 		
 		//Se pasa el segundo valor como 0 y tiene que devolver 0
-		r1 = real1;
-		r2 = CERO;
-		assertEquals(CERO,Division.divReales(r1, r2));
+		assertEquals(CERO,Division.divReales(real1, real2));
 		
 		//Se pasa el primer valor como 0 y tiene que devolver el resultado de la 
 		//operacion
-		r1 = CERO;
-		r2 = real2;
-		result = r1/r2;
-		assertEquals(result, Division.divReales(r1, r2));
+		result = CERO/real2;
+		assertEquals(result, Division.divReales(real1, real2));
 	}
 	
 	/**
@@ -101,39 +93,32 @@ public class TestDivision {
 	@Test
 	public void testDivEnteros() {
 		System.out.println("***** Test metodo divEnteros *****");
-		int r1 = num1;;
-		int r2 = num2;;
-		double result =(double)r1/r2;
+		
 		//Se pasan dos valores enteros positivos y tiene que devolver el valor de 
 		//la operacion
-		assertEquals(result, Division.divEnteros(r1,r2));
+		double result =(double)entero1/entero2;
+		assertEquals(result, Division.divEnteros(entero1,entero2));
 		
 		//Se pasa el primer valor como negativo y el segundo como positivo y tiene
 		//que devolver 0
-		r1 = -r1;
-		assertEquals(CERO, Division.divEnteros(r1, r2));
+		int enteroNegativo1 = -entero1;
+		assertEquals(CERO, Division.divEnteros(enteroNegativo1, entero2));
 		
 		//Se pasa el primer valor como positivo y el segundo como negativo y tiene que
 		//devolver 0
-		r1 = num1;
-		r2 = -r2;
-		assertEquals(CERO, Division.divEnteros(r1,r2));
+		int enteroNegativo2 = -r2;
+		assertEquals(CERO, Division.divEnteros(entero1,enteroNegativo2));
 		
 		//Se pasan los dos valores negativos y tiene que devolver 0
-		r1 = -r1;
-		assertEquals(CERO, Division.divEnteros(r1, r2));
+		assertEquals(CERO, Division.divEnteros(enteroNegativo1, enteroNegativo2));
 		
 		//Se pasa el segundo valor como 0 y tiene que devolver 0
-		r1 = num1;
-		r2 = CERO;
-		assertEquals(CERO,Division.divReales(r1, r2));
+		assertEquals(CERO,Division.divReales(entero1, CERO));
 		
 		//Se pasa el primer valor como 0 y tiene que devolver el resultado de la 
 		//operacion
-		r1 = CERO;
-		r2 = num2;
-		result =(double)r1/r2;
-		assertEquals(result, Division.divReales(r1, r2));
+		result =(double)entero1/entero2;
+		assertEquals(result, Division.divReales(entero1, entero2));
 	}
 	
 	/**
@@ -143,33 +128,30 @@ public class TestDivision {
 	@Test
 	public void testInverso() {
 		System.out.println("***** Test metodo testInverso *****");
-		double r1 = real1;
-		int r2 = num1;
 		
 		//sacamos el inverso de un numero real dado
-		double result = 1/r1;
-		assertEquals(result, Division.inverso(r1));
+		double result = 1/real1;
+		assertEquals(result, Division.inverso(real1));
 		
 		//sacamos el inverso de un numero entero dado
-		result = (double)1/r2;
-		assertEquals(result, Division.inverso(r2));
+		result = (double)1/entero2;
+		assertEquals(result, Division.inverso(entero2));
 		
 		//sacamos el numero que se pasa como argumento es 0, devuelve un mensaje de 
 		//que el divisior no puede ser 0 y retorna 0.
-		r1 = CERO;
-		result = 1/r1;
-		assertNotEquals(result, Division.inverso(r1));
-		assertEquals(CERO, Division.inverso(r1));
+		result = 1/CERO;
+		assertNotEquals(result, Division.inverso(real1));
+		assertEquals(CERO, Division.inverso(real1));
 		
 		/*
 		 * sacamos el inverso de un numero negativo. Como la calculadora no admite
 		 * negativos, lanza un mensaje de que solo se admiten numeros positivos y
 		 * retorna 0
 		 */
-		r1 = -real1;
-		result = 1/r1;
-		assertNotEquals(result, Division.inverso(r1));
-		assertEquals(CERO, Division.inverso(r1));
+		double realNegativo = -real1;
+		result = 1/realNegativo;
+		assertNotEquals(result, Division.inverso(realNegativo));
+		assertEquals(CERO, Division.inverso(realNegativo));
 		}
 	
 	/**
@@ -179,22 +161,19 @@ public class TestDivision {
 	@Test
 	public void testRaiz() {
 		System.out.println("***** Test metodo raiz *****");
-		double r1 = real1;
-		int r2 = num1;
-		
+	
 		//sacamos la raiz cuadrada de un numero real dado
-		double result = Math.sqrt(r1);
-		assertEquals(result, Division.raiz(r1));
+		double result = Math.sqrt(real1);
+		assertEquals(result, Division.raiz(real1));
 		
 		//sacamos la raiz de un numero entero dado
-		result = (double)(Math.sqrt(r2));
-		assertEquals(result, Division.raiz(r2));
+		result = (double)(Math.sqrt(entero2));
+		assertEquals(result, Division.raiz(entero2));
 		
 		//Se pasa como argumento 0 para calcualr la raiz cuadrada, devuelve un mensaje de 
 		//que el numero pasado no puede ser 0 y retorna 0.
-		r1 = CERO;
-		result = Math.sqrt(r1);
-		assertEquals(result, Division.raiz(r1));
+		result = Math.sqrt(CERO);
+		assertEquals(result, Division.raiz(CERO));
 		
 		
 		/*
@@ -202,10 +181,9 @@ public class TestDivision {
 		 * negativos, lanza un mensaje de que solo se admiten numeros positivos y
 		 * retorna 0
 		 */
-		r1 = -real1;
-		result = 1/r1;
-		assertNotEquals(result, Division.raiz(r1));
-		assertEquals(CERO, Division.raiz(r1));
+		double realNegativo = -real1;
+		assertNotEquals(result, Division.raiz(realNegativo));
+		assertEquals(CERO, Division.raiz(realNegativo));
 		
 		}
 
